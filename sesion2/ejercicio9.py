fdabd71030b7529ce2c264610e859b0e46f7164c
Replit user <>
2022-08-09 15:10:37 +0000

    Ejercicios de la segunda sesion

---
+++ sesion2/ejercicio9.py
@@ -0,0 +1,13 @@
+'''
+*********** Curso de programación acelerada en Python ************
+Date 05-08-2022
+File: sesion2/ejercicio9.py
+Autor: jromeroc
+Action: preguntar contraseña
+'''
+PASSWORD = "98765"
+contraseña = str(input("Escriba su contraseña: "))
+if contraseña == PASSWORD:
+  print("¡contraseña correcta!")
+else:
+  print("¡contraseña incorrecta!")
